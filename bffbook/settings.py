24650d44789e58f8c7570570bd724ef28b5a3786
Łukasz Mąkinia <lukaszmakinia@iMac-ukasz.local>
2020-09-27 20:50:57 +0200

    part 17

--- bffbook/settings.py
+++ bffbook/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'profiles.context_processors.profile_pic',
+                'profiles.context_processors.invatations_received_no',
             ],
         },
     },
---
+++ profiles/context_processors.py
@@ -0,0 +1,15 @@
+from .models import Profile, Relationship
+
+def profile_pic(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        pic = profile_obj.avatar
+        return {'picture':pic}
+    return {}
+
+def invatations_received_no(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        qs_count = Relationship.objects.invatations_received(profile_obj).count()
+        return {'invites_num':qs_count}
+    return {}
